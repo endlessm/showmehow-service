abed22c2b28557b8d2987c04ba5cc9e36b88fcd0
Sam Spilsbury <sam@endlessm.com>
2016-09-06 15:41:27 +0800

    validator: Use the 'errors' value from validateDescriptors directly
    The "valid" value more to do with whether the descriptors file
    could be loaded at all - for the validator, we care about whether
    there were any errors.

--- showmehow-validator.js
+++ showmehow-validator.js
@@ -23,7 +23,7 @@ const Validation = imports.lib.validation;
  */
 function validateFile(filename) {
     const contents = Gio.File.new_for_path(filename).load_contents(null)[1];
-    const [valid, errors] = Validation.validateDescriptors(JSON.parse(contents));
+    const errors = Validation.validateDescriptors(JSON.parse(contents))[1];
     errors.forEach(e => log("lesson validation error: " + e));
     return errors.length;
 }
